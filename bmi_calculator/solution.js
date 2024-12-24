1259a5688a8e531c2087bd793ef5656999a2c17c
Lohith Reddy Poreddy <96791230+LohithReddyPoreddy@users.noreply.github.com>
2024-12-24 23:06:45 +0000

    changes to solution.js

--- bmi_calculator/solution.js
+++ bmi_calculator/solution.js
@@ -1,9 +1,10 @@
 const form = document.querySelector("form")
 
 form.addEventListener('submit', (e)=>{
+    e.preventDefault();
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
-    const result = document.querySelector('#result');
+    const result = document.querySelector('#results');
 
     if(height === '' || height < 0 || isNaN(height)){
         result.innerHTML = "Please enter a valid height";
